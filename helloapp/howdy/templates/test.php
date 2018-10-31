1154d5df1b73d26ab7389d7c2dd0965ff22ece86
acrosight <30530924+acrosight@users.noreply.github.com>
2018-10-31 18:41:02 +0100

    Add files via upload

---
+++ helloapp/howdy/templates/test.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$variabel = "Heisann";
+$nummer = 12;
+
+echo "The number is " . $number . "."
+?>
---
+++ helloapp/howdy/views.py
@@ -0,0 +1,14 @@
+# howdy/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+# Add this view
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class MafiaPageView(TemplateView):
+    template_name = "mafia.html"
